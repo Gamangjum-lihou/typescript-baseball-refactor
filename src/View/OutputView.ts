f0ac297c4cf808d1fcf674c63f303ad0320eea0f
ryan-dia <cjfdnjs158@naver.com>
2023-05-13 18:50:45 +0900

    feat(View) : 에러 출력 메서드 및 게임종료 메서드 추가
    - printError  :  에러출력
    - printGameEnd : 게임종료 메시지 출력 및 게임 종료

--- src/View/OutputView.ts
+++ src/View/OutputView.ts
@@ -1,8 +1,22 @@
 import { Console } from '@woowacourse/mission-utils';
+import { OUTPUT_MESSAGE } from '../Constants/Message';
+
+interface IError extends Error {
+  cause: string;
+}
 
 const OutputView = {
   printStart() {
     Console.print('숫자 야구 게임을 시작합니다.');
+  },
+
+  printError({ name, message, cause }: IError) {
+    Console.print(OUTPUT_MESSAGE.error(name, message, cause));
+  },
+
+  printGameEnd() {
+    Console.print(OUTPUT_MESSAGE.end);
+    Console.close();
   },
 };
 
